ce76349a5e6ca0f46664416ae986042f86b73b0c
Josh (Seungjun) Kim <juny1226@gmail.com>
2019-01-29 14:54:18 +0000

    Update and rename Khan Academy_Making your own grocery database.sql to Khan Academy_Making your own clothing store database.sql

--- Khan Academy_Making your own clothing store database.sql
+++ Khan Academy_Making your own clothing store database.sql
@@ -1,3 +1,10 @@
+-- Practice Making your own Store Database! (Khan Academy)
+-- Create your own store! Your store should sell one type of things, 
+-- like clothing or bikes, whatever you want your store to specialize in. 
+-- You should have a table for all the items in your store, and at least 5 columns for the kind of data you think you'd need to store. 
+-- You should sell at least 15 items, and use select statements to order your items by price and show at least one statistic about the 
+-- items.
+
 CREATE TABLE store (id INTEGER PRIMARY KEY, name TEXT,
 target_gender TEXT, fabric TEXT, price NUMERIC);
 
